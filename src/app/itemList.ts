3d1276709217bc95fda03ee7e25837fe1cfa9ea7
Alvin Cheong <alvincheong@u.nus.edu>
2020-12-06 06:26:00 +0800

    updated p list

--- src/app/itemList.ts
+++ src/app/itemList.ts
@@ -1,10 +1,10 @@
 export var ITEMLIST = [
-    {id:0, name:'Fandi', quantity:0, imgUrl: '/assets/Fendi.JPG', price: 3038},
-    {id:1,name: 'Hermènsès', quantity: 0, imgUrl: '/assets/kelly.jpg', price: 379261},
-    {id:2,name: 'Hiewe', quantity: 0, imgUrl: '/assets/Loewe.JPG', price: 3700},
-    {id:3,name: 'Tori Bird', quantity: 0, imgUrl: '/assets/Tory.JPG', price: 331.28},
-    {id:4,name: 'Herpès (passed down)', quantity: 0, imgUrl: '/assets/Birkin.jpg', price: 30354},
-    {id:5,name: 'Prata', quantity: 0, imgUrl: '/assets/Prada.JPG', price: 2639.50},
+    {id:0, name:'Fandi', quantity:0, imgUrl: '/assets/Fendi.JPG', price: 3038, commaPrice: "3,308"},
+    {id:1,name: 'Hermènsès', quantity: 0, imgUrl: '/assets/kelly.jpg', price: 379261, commaPrice: "379,261"},
+    {id:2,name: 'Hiewe', quantity: 0, imgUrl: '/assets/Loewe.JPG', price: 3700, commaPrice: "3,700"},
+    {id:3,name: 'Tori Bird', quantity: 0, imgUrl: '/assets/Tory.JPG', price: 331.28, commaPrice: "331.28"},
+    {id:4,name: 'Herpès (passed down)', quantity: 0, imgUrl: '/assets/Birkin.jpg', price: 30354, commaPrice: "30,354"},
+    {id:5,name: 'Prata', quantity: 0, imgUrl: '/assets/Prada.JPG', price: 2639.50, commaPrice: "2,639.50"},
   ];
 
 
